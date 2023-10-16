3a2aa2601fc6df67ac1aedd356d3ddb50c4860c9
kaadon.com <kaadon.com@gmail.com>
2023-10-16 10:33:19 +0800

    add logo

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 export function returnError(message = "Failure request",code = 201){
     return {code:code,message:message,success: false}
 }
-export function returnSuccess(data = {},message = "Successful request",code = 0){
+export function returnSuccess(data = {},message = "Successful request",code = 200){
     return  {code:code,message:message,success: true,data:data}
 }
